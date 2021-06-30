be61f21ec4647d8adec2832a2705f7c4dc7b3374
Kriska939 <cristinacorrado93@gmail.com>
2021-06-30 17:12:44 +0200

    added basic variables

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
-console.log("Prova Collegamento");
+/* ISTRUZIONI SCRIPT:
+ 1 - Chiedi all’utente il suo nome,
+ 2 - poi chiedi il suo cognome,
+ 3 - poi chiedi il suo colore preferito
+ Infine scrivi sulla pagina nomecognomecolorepreferito21 */
+
+//  Recupero informazioni da utente:
+
+var name = prompt('What\'s your name?', 'Cristina');
+var surname = prompt('What\'s your surname?', 'Corrado');
+var favoriteColor = prompt('What\'s your favorite color?', 'Lilac');
+
+// Rendo 21 var stringa
+
+var pwEnd = '21';
+
